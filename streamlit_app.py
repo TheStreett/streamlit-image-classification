2180afd1c0ed3b0dcfd9deaaf6aea00e8f991c82
Praditya Raudi <pradityaraudi@gmail.com>
2022-03-20 11:25:36 -0400

    remove unnecessary logging, add comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import ast
 import json
 import base64
+import logging
 import zipfile
 from io import BytesIO
 from collections import Counter
@@ -8,65 +9,54 @@ from collections import Counter
 import requests
 from PIL import Image
 import streamlit as st
-from loguru import logger
 import plotly.express as px
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
 
-# Redirect logger into streamlit output
-def redirect_loguru_to_streamlit():
-    def _filter_warning(record):
-        return record["level"].no == logger.level("WARNING").no    
-    if 'warning_logger' not in st.session_state:
-        st.session_state['warning_logger'] = logger.add(st.warning, filter=_filter_warning, level='INFO')
-    if 'error_logger' not in st.session_state:
-        st.session_state['error_logger'] = logger.add(st.error, level='ERROR')
-
-redirect_loguru_to_streamlit()
-
 def download_data_sample(api_url, token):
-    # try:
-    # Set the path for eval API
-    eval_url = api_url + "/prod/eval"
-    
-    # Set the authorization based on query parameter 'token', 
-    # it is obtainable once you logged in to the modelshare website
-    headers = {
-        "Content-Type": "application/json", 
-        "authorizationToken": token,
-    }
+    try:
+        # Set the path for eval API
+        eval_url = api_url + "/prod/eval"
+        
+        # Set the authorization based on query parameter 'token', 
+        # it is obtainable once you logged in to the modelshare website
+        headers = {
+            "Content-Type": "application/json", 
+            "authorizationToken": token,
+        }
 
-    # Set the body indicating we want to get sample data from eval API
-    data = {
-        "exampledata": "TRUE"
-    }
-    data = json.dumps(data)
+        # Set the body indicating we want to get sample data from eval API
+        data = {
+            "exampledata": "TRUE"
+        }
+        data = json.dumps(data)
 
-    # Send the request
-    sample_images = requests.request("POST", eval_url, 
-                                     headers=headers, data=data).json()
+        # Send the request
+        sample_images = requests.request("POST", eval_url, 
+                                         headers=headers, data=data).json()
 
-    # Parsing the base64 encoded images
-    images = sample_images['exampledata'].split(",")
+        # Parsing the base64 encoded images
+        images = sample_images['exampledata'].split(",")
 
-    # Prepare the data sample in zip
-    zip_buffer = BytesIO()
-    with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zip_file:
-        for i, image in enumerate(images):
-            file_name = "image_sample{}.png".format(i)
-            image_buffer = BytesIO()
-            image_buffer.write(base64.b64decode(image))
-            zip_file.writestr(file_name, image_buffer.getvalue())
-    
-    btn = st.download_button(
-        label="Download data sample",
-        data=zip_buffer.getvalue(),
-        file_name="data_sample.zip",
-        mime="application/zip"
-    )
-    # except Exception as e:
-    #     logger.error(e)
+        # Prepare the data sample in zip
+        zip_buffer = BytesIO()
+        with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zip_file:
+            for i, image in enumerate(images):
+                file_name = "image_sample{}.png".format(i)
+                image_buffer = BytesIO()
+                image_buffer.write(base64.b64decode(image))
+                zip_file.writestr(file_name, image_buffer.getvalue())
+        
+        # Setup a download button
+        btn = st.download_button(
+            label="Download data sample",
+            data=zip_buffer.getvalue(),
+            file_name="data_sample.zip",
+            mime="application/zip"
+        )
+    except Exception as e:
+        logging.error(e)
 
 def display_result(images, labels, statuses):
     status_label = {
@@ -99,77 +89,80 @@ def display_bar_chart(freqs, labels):
     st.plotly_chart(fig, use_container_width=True)
     
 def display_stats(labels):
-    # Pie chart, where the slices will be ordered and plotted counter-clockwise:
     counter = Counter(labels)
     unique_labels = list(counter.keys())
-    freqs = list(counter.values())
+    freqs = list(counter.values()) # frequency of each labels
 
+    # Size or portion in pie chart
     sizes = [float(x) / sum(freqs) * 100 for x in freqs]
 
     display_pie_chart(sizes, unique_labels)
     display_bar_chart(freqs, unique_labels)
 
-# Set the API url accordingly based on AIModelShare Playground API.
-api_url = "https://5l7clbsyu5.execute-api.us-east-1.amazonaws.com"
+def main():
+    # Set the API url accordingly based on AIModelShare Playground API.
+    api_url = "https://5l7clbsyu5.execute-api.us-east-1.amazonaws.com"
 
-# Get the query parameter
-params = st.experimental_get_query_params()
-token = params['token'][0]
+    # Get the query parameter
+    params = st.experimental_get_query_params()
+    token = params['token'][0]
 
-st.header("Flower Image Classification")
-st.write("Choose any image and get the prediction:")
+    st.header("Flower Image Classification")
+    st.write("Choose any image and get the prediction:")
 
-uploaded_files = st.file_uploader(
-    label="Choose an image...",
-    type=["png", "jpg", "jpeg"],
-    accept_multiple_files=True,
-)
+    uploaded_files = st.file_uploader(
+        type=["png", "jpg", "jpeg"],
+        accept_multiple_files=True,
+    )
 
-download_data_sample(api_url, token)
-    
-labels = []
-statuses = []
-images = []    
-if uploaded_files:
-    for uploaded_file in uploaded_files:
-        try:
-            # Prepare the uploaded image into base64 encoded string
-            images.append(uploaded_file)
-            image = Image.open(uploaded_file)
-            buffered = BytesIO()
-            image.save(buffered, format="JPEG")
-            encoded_string = base64.b64encode(buffered.getvalue())
-            data = json.dumps({"data": encoded_string.decode('utf-8')})
+    download_data_sample(api_url, token)
+        
+    labels = []
+    statuses = []
+    images = []    
+    if uploaded_files:
+        for uploaded_file in uploaded_files:
+            try:
+                # Prepare the uploaded image into base64 encoded string
+                images.append(uploaded_file)
+                image = Image.open(uploaded_file)
+                buffered = BytesIO()
+                image.save(buffered, format="JPEG")
+                encoded_string = base64.b64encode(buffered.getvalue())
+                data = json.dumps({"data": encoded_string.decode('utf-8')})
 
-            # Set the path for prediction API
-            pred_url = api_url + "/prod/m"
-            
-            # Set the authorization based on query parameter 'token', 
-            # it is obtainable once you logged in to the modelshare website
-            headers = {
-                "Content-Type": "application/json", 
-                "authorizationToken": token,
-            }
+                # Set the path for prediction API
+                pred_url = api_url + "/prod/m"
+                
+                # Set the authorization based on query parameter 'token', 
+                # it is obtainable once you logged in to the modelshare website
+                headers = {
+                    "Content-Type": "application/json", 
+                    "authorizationToken": token,
+                }
 
-            # Send the request
-            prediction = requests.request("POST", pred_url, 
-                                          headers=headers, data=data)
+                # Send the request
+                prediction = requests.request("POST", pred_url, 
+                                              headers=headers, data=data)
 
-            # Parse the prediction
-            label = ast.literal_eval(prediction.text)[0]
-            
-            # Insert the label into labels
-            labels.append(label)
-            
-            # Insert the API call status into statuses
-            statuses.append(True)
-        except Exception as e:
-            logger.error(e)
+                # Parse the prediction
+                label = ast.literal_eval(prediction.text)[0]
+                
+                # Insert the label into labels
+                labels.append(label)
+                
+                # Insert the API call status into statuses
+                statuses.append(True)
+            except Exception as e:
+                logging.error(e)
 
-            # add label as None if necessary
-            if len(labels) < len(images):
-                labels.append(None)
-            statuses.append(False)
+                # add label as None if necessary
+                if len(labels) < len(images):
+                    labels.append(None)
+                statuses.append(False)
 
-    display_result(images, labels, statuses)
-    display_stats(labels)
+        display_result(images, labels, statuses)
+        display_stats(labels)
+
+if __name__ == "__main__":
+    main()
