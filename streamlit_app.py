f09143a2e498e4a0fd5f35210f568b26a6a22309
Praditya Raudi <pradityaraudi@gmail.com>
2022-03-20 11:19:22 -0400

    debug fix download data

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,8 +56,7 @@ def download_data_sample(api_url, token):
         for i, image in enumerate(images):
             file_name = "image_sample{}.png".format(i)
             image_buffer = BytesIO()
-            with image_buffer as ib:
-                ib.write(base64.b64decode(image))
+            image_buffer.write(base64.b64decode(image))
             zip_file.writestr(file_name, image_buffer.getvalue())
     
     btn = st.download_button(
