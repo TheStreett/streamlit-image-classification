8120cd6a751b3fc7da6ed2da5d1aefdc36ad27cf
Praditya Raudi <pradityaraudi@gmail.com>
2022-03-28 01:46:16 -0400

    add css

--- streamlit_app.py
+++ streamlit_app.py
@@ -222,7 +222,15 @@ def transform_image(uploaded_file):
     image.thumbnail(MAX_SIZE)
     return (image, uploaded_file.name)
 
+# Add custom CSS for streamlit component
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
 def main():
+    # Set custom CSS for whole layout
+    local_css("style.css")
+
     # Set the API url accordingly based on AIModelShare Playground API.
     api_url = "https://5l7clbsyu5.execute-api.us-east-1.amazonaws.com"
 
