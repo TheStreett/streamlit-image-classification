3d20e926f35c1e3fd21da0358930776f0ba92c3d
Praditya Raudi <pradityaraudi@gmail.com>
2022-04-02 14:43:23 -0400

    add image on dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ def display_result(images, labels, statuses, datetimes, uuids):
         unsafe_allow_html=True
     )
 
-    logging.warning(data_frame.head())
+    logging.warning(data_frame.columns)
     data_frame = data_frame.drop(['input'])
     data_frame = data_frame.assign(unique_id=uuids)
 
